8f0434f0483e8aa3a25f11f348361992ce171fbe
thegame1432 <harshpandiyar1432@gmail.com>
2025-02-08 00:01:47 +0530

    Built browse page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Home from './components/Home'
 import Jobs from './components/Jobs';
+import Browse from './components/Browse'
 
 const appRouter = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const appRouter = createBrowserRouter([
   {
     path:'/jobs',
     element:<Jobs/>
+  },
+  {
+    path:'/browse',
+    element:<Browse/>
   },
 ]);
 
